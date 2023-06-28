ee4a84eadb08d5c7f778fb13ef8bc03957b98680
Aleksandar Mihajlovic <aleksandar.mihajlovic96@yahoo.com>
2023-06-29 01:58:15 +0200

    Circle around cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+    const cursor = document.querySelector(".cursor") as HTMLElement;
+
+    document.addEventListener("mousemove", (e) => {
+      const leftPosition = e.pageX;
+      const topPosition = e.pageY;
+
+      cursor.style.left = `${leftPosition}px`;
+      cursor.style.top = `${topPosition}px`;
+    });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="cursor"></div>
       </header>
     </div>
   );
